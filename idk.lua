4f1667450e0049b74c56f354e5f682c059b1fbc5
sigmaboy-sigma-boy <tochnonepupsik228345@gmail.com>
2025-07-06 16:11:48 +0200

    Add files via upload

---
+++ idk.lua
@@ -0,0 +1,8 @@
+_G.SnowFarm = true
+_G.FarmSlapsSnowFarm = 100
+_G.WaitRegisterSnowFarm = 1
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/OpenSourceScripts/refs/heads/main/SlapFarmGui/Versions/(v4.3).luau'))()
+
+_G.ErrorFarm = true
+_G.FarmSlapsErrorFarm = 100
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/OpenSourceScripts/refs/heads/main/SlapFarmGui/Versions/(v4.3).luau'))()
